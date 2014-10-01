b771f37f169e0d2a4d6e273468edacc4a2670266
perehn <per.ehnbom@gmail.com>
2014-10-01 12:07:21 +0200

    new fixture

--- app-source/app/models/fixtures.js
+++ app-source/app/models/fixtures.js
@@ -19,11 +19,12 @@ function(){
 	
 	
 	var issueStore = initStore([
-	    {title : "Look over tests", description : 'We need to look into tests, right now there are none. I guess at least one test wouldnt hurt.', status : "todo", 
+	    {title : "Test in Android browser", description : 'Test the app on chrome in Android', status : "todo", 
 	    	comments : [{date : new Date(), text : 'We should look into this'}, {date : new Date(), text : 'We definitely should'}]},     	          		    
 	    {title : "Get ready for Iphone and Android", description : 'Use phonegap and publish the app for Iphone and Android', status : "todo"},
-	    {title : "Refactor the code", description : 'We need to refactor the code base', status : "inprogress"},
-	    {title : "Post app on Framework7 forum", description : 'Post comment in the forum', status : "inprogress"}
+	
+	    {title : "Post app on Framework7 forum", description : 'Post comment in the forum', status : "inprogress"},
+	    {title : 'Setup production build', description : 'Setup a build with RequireJS. Should result in a minified app.js and main.css', status : 'done', comments : [{date : new Date(2014, 8, 29), text: 'Done. use grunt build'}]}
 	  ]);
 	
 	can.fixture({
