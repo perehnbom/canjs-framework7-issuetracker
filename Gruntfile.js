f1901787c6cbdd91ee7fb2a4c57c7487d3a14d1b
Per Ehnbom <per.ehnbom@consult.nordea.com>
2015-02-25 10:49:33 +0100

    gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,16 +36,31 @@ module.exports = function(grunt) {
             nospawn: true
           }
         }
+      },
+      connect: {
+        server: {
+          options: {
+            port: 9001,
+            base: '',
+            keepalive:true
+          }
+        }
       }
   });
+
+
+ 
 
   // Load tasks from NPM
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
   // Default task.
   grunt.registerTask('build', ['clean', 'requirejs', 'less']);
   grunt.registerTask('default', ['less','watch']);
+  grunt.registerTask('server', ['less','watch', 'connect']);
+
 };
